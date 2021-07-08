ad0d2666ab97d6f58f9d65a512c411aaf9acba9e
kurt <kurt.schaefer042@gmail.com>
2021-07-08 13:33:43 -0500

    basic authentication works

--- authentication/views.py
+++ authentication/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render, reverse, redirect
 from django.http.response import HttpResponseRedirect
-from django.contrib.auth import authenticate, login,logout
+from django.contrib.auth import authenticate, login, logout
 from authentication.forms import LoginForm
 
 # Create your views here.
+
 
 def login_view(request):
   if request.method == "POST":
     form = LoginForm(request.POST)
     if form.is_valid():
-      data=form.cleaned_data
-      user=authenticate(
+      data = form.cleaned_data
+      user = authenticate(
         request,
         username=data['username'],
         password=data['password']
@@ -19,7 +20,6 @@ def login_view(request):
         login(request, user)
         user.save()
         return HttpResponseRedirect(request.GET.get("next", "/"))
-
   form = LoginForm()
   return render(request, 'generic_form.html', {'form': form})
 
@@ -30,7 +30,7 @@ def logout_view(request):
 
 
 def index_view(request):
-  if request.user.is_authenticated:
-    placeholder = "Placeholder.objects.order_by('post_created')"
-    return render(request, 'index.html', {'incoming': placeholder})
-  return HttpResponseRedirect(reverse('login'))
+    if request.user.is_authenticated:
+        placeholder = "Placeholder.objects.order_by('post_created')"
+        return render(request, 'index.html', {'incoming': placeholder})
+    return HttpResponseRedirect(reverse('login'))
--- planetgram/urls.py
+++ planetgram/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index_view, name='home'),
     path('login/', views.login_view, name='login'),
-    path('logout', views.logout_view)
+    path('logout/', views.logout_view)
 ]
