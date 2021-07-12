11674057bc62adea80e35b25c97e8c5ef3a6ffd2
kurt <kurt.schaefer042@gmail.com>
2021-07-12 11:49:26 -0500

    basic posts show on front

--- planetuser/views.py
+++ planetuser/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from .models import MyUser
-
+from planetpost.models import Planet_Post
 
 def index_view(request):
-  message="hello galaxy"
-  return render(request, 'index.html', {'incoming': message})
+  if request.user.is_authenticated:
+    posts=Planet_Post.objects.all()
+  return render(request, 'index.html', {'incoming':posts})
 
+def profile_view(request, user_id):
+  my_user= MyUser.object.get(id=user_id)
+  
